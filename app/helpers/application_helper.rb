ae007edd8d855df07ab6a4af7c563ca56762b83f
david liu <david.liu@autodesk.com>
2014-09-13 10:34:22 -0700

    avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def avatar_url(email)
+		gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    	return "http://gravatar.com/avatar/#{gravatar_id}.png"
+		# return 'http://gra'
+	end
 end
