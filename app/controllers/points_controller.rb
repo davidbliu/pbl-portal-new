edd0ce4e06e5907c3d108e186a692c187fd15e6a
david liu <david.liu@autodesk.com>
2014-09-30 17:37:01 -0700

    change num events

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -35,7 +35,7 @@ class PointsController < ApplicationController
 	end
 
 	def mark_attendance
-		@semester_events = Event.this_semester.paginate(:page => params[:page], :per_page => 10)
+		@semester_events = Event.this_semester.paginate(:page => params[:page], :per_page => 20)
 		# @semester_events = Event.this_semester
 		if current_member.admin? 
 			@current_cms = Member.current_members
