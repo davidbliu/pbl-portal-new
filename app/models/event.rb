46d495dd08d91e5e2776ce776f846043f7ed1533
david liu <david.liu@autodesk.com>
2014-12-29 22:29:34 -0800

    pull google events changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  # attr_accessible :name, :start_time, :end_time, :description, :semester_id, :google_id
+  attr_accessible :name, :start_time, :end_time, :description, :semester_id, :google_id
   belongs_to :semester, foreign_key: :semester_id
   has_one :event_points #, dependent: :destroy
   has_many :event_members #, dependent: :destroy
