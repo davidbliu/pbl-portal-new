1469fe22b15539c438429eb72fc58c1d1da9ea45
david liu <david.liu@autodesk.com>
2015-01-09 23:04:21 -0800

    prepare semester for delib commit

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -2,13 +2,13 @@ class Semester < ActiveRecord::Base
   attr_accessible :end_date, :name, :start_date
   # has_many :members, through: :semester_members
   # has_many :event_points
-  has_many :committee_members
-  has_many :events
-  has_many :event_members
+  has_many :committee_members, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :event_members, dependent: :destroy
+  has_many :deliberations, dependent: :destroy
+
 
   def self.current_semester
-  	# return Semester.first(:order => 'start_date DESC')
     return Semester.order(:start_date).reverse.first
-  	# return Semester.first
   end
 end
