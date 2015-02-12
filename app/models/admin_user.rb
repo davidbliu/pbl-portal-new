105321845e7f95956461f5dda976d2878808ef2f
david liu <david.liu@autodesk.com>
2015-02-11 18:27:24 -0800

    can create new admins

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,8 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessible :email, :password, :password_confirmation
+
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 end
