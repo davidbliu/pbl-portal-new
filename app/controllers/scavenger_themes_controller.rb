68162e8b4ac0796f47c912e83c3a32929d85565e
david liu <david.liu@autodesk.com>
2015-02-20 00:54:01 -0800

    started modifying controller

--- app/controllers/scavenger_themes_controller.rb
+++ app/controllers/scavenger_themes_controller.rb
@@ -9,13 +9,18 @@ class ScavengerThemesController < ApplicationController
 	end
 
 	def upload_photo
-		@theme = ScavengerTheme.find(params[:id])
+		@group = ScavengerGroup.find(params[:id])
+		@theme = ScavengerTheme.find(@group.theme_id)
 		photo = ScavengerPhoto.new
 		photo.image = params[:image]
 		# find this persons group and upload the photo for my group
-		photo.member_id = current_member.id
+		photo.group_id = @group.id
 		photo.save!
 		@theme.scavenger_photos << photo
 		render :nothing => true, :status => 200, :content_type => 'text/html'
+	end
+
+	def generate_groups
+		@theme = ScavengerTheme.find(params[:id])
 	end
 end
