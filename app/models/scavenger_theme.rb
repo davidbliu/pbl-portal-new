10f24f99c5a57a4f18077e020e16d10c4bd5f952
Kevin Zhang <kzhang.kz@gmail.com>
2015-02-21 12:47:14 -0800

    Make sure no group less than 5. Random shuffling of groups

--- app/models/scavenger_theme.rb
+++ app/models/scavenger_theme.rb
@@ -9,8 +9,15 @@ class ScavengerTheme < ActiveRecord::Base
 		p 'generating groups'
 		people = Member.current_cms.to_a
 		num_groups = (people.length / 5).floor
+
+		extra = people.length % 5
+
+
+		#temp array of reordered people
+		people = people.shuffle
+
 		# create groups
-		for i in 0..num_groups
+		for i in 0..num_groups-extra-1
 			group = ScavengerGroup.new
 			group.scavenger_theme_id = self.id
 			group.name = 'group'+Random.rand(1000000).to_s
@@ -24,6 +31,26 @@ class ScavengerTheme < ActiveRecord::Base
 				gm.scavenger_groups_id = group.id
 				gm.save!
 			end
+		end
+		#odd group
+		if extra != 0
+			for i in 0..extra-1
+				group = ScavengerGroup.new
+				group.scavenger_theme_id = self.id
+				group.name = 'group'+Random.rand(1000000).to_s
+				# add people to group
+				group_people = people[0, 6]
+				people  =people[6, people.length]
+				group.save!
+				for p in group_people
+					gm = ScavengerGroupMember.new
+					gm.member_id = p.id
+					gm.scavenger_groups_id = group.id
+					gm.save!
+				end
+			end
+			
+
 		end
 	end
 
