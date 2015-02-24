5d21a88af9d3c983f652531bca2cc9520b319fe8
david liu <david.liu@autodesk.com>
2015-02-23 22:48:10 -0800

    remove dependent destroy

--- app/models/scavenger_photo.rb
+++ app/models/scavenger_photo.rb
@@ -2,7 +2,7 @@ class ScavengerPhoto < ActiveRecord::Base
 	attr_accessible :name, :description, :image, :scavenger_theme_id, :member_id, :group_id, :points,:confirmation_status
 
 	belongs_to :scavenger_theme
-	belongs_to :scavenger_group, :foreign_key=>:group_id, :dependent=>:destroy
+	# belongs_to :scavenger_group, :foreign_key=>:group_id, :dependent=>:destroy
 
 	def group
 		return ScavengerGroup.find(self.group_id)
