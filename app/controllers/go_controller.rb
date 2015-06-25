0712722c2c14c6eadddfc17b5f97da4863eb6408
David Liu <davidbliu@gmail.com>
2015-06-24 21:57:12 -0700

    generic metrics

--- app/controllers/go_controller.rb
+++ app/controllers/go_controller.rb
@@ -6,24 +6,27 @@ class GoController < ApplicationController
 			@message = 'No key was provided'
 		elsif go_hash.keys.include?(go_key)
 			golink = go_hash[go_key]
+			# log click tracking data
 			if current_member	
 				click = GoLinkClick.new()
 				click.member_id = current_member.id
 				click.go_link_id = golink.id
+				click.go_link_key = golink.key
 				click.save!
 			else
 				click = GoLinkClick.new()
 				click.member_id = -1
 				click.go_link_id = golink.id
+				click.go_link_key = golink.key
 				click.save!
 			end
+			# send to link url
 			redirect_to golink.url
 		else
 			@message = 'The key ('+go_key.to_s+') was not recognized, please check the catalogue to make sure your key exists!'
 		end
 		# else display the catalogue
 		@go_key = go_key
-		# @go_links = GoLink.all.order(:key)
 	end
 
 	def catalogue
--- app/controllers/go_link_clicks_controller.rb
+++ app/controllers/go_link_clicks_controller.rb
@@ -4,5 +4,16 @@ class GoLinkClicksController < ApplicationController
 		@member_hash = Member.member_hash
 		@member_hash[-1] = Member.new(name: "Not Logged In")
 		@go_link_id_hash = GoLink.go_link_id_hash
+
+		@link_clicks_hash = Hash.new
+		@go_link_clicks.each do |click|
+			if not @link_clicks_hash.keys.include?(click.go_link_id)
+				@link_clicks_hash[click.go_link_id] = Hash.new
+			end
+			if not @link_clicks_hash[click.go_link_id].keys.include?(click.member_id)
+				@link_clicks_hash[click.go_link_id][click.member_id] = Array.new
+			end
+			@link_clicks_hash[click.go_link_id][click.member_id] << click
+		end
 	end
 end
