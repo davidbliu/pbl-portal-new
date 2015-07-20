8bb731a06fe0070acf1407649b0620a9130e6d80
David Liu <davidbliu@gmail.com>
2015-07-20 11:50:46 -0700

    add to guide

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,7 @@ module ApplicationHelper
     	return "http://gravatar.com/avatar/#{gravatar_id}.png"
 	end
 
+	def loading_image
+		"http://wpc.077d.edgecastcdn.net/00077D/fender/images/2013/template/drop-nav-loader.gif"
+	end
 end
