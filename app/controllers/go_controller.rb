3f0d1c7cb2c019e8232552e1e8f03ff0ccfa46da
David Liu <davidbliu@gmail.com>
2015-08-10 21:41:40 -0700

    add email tracking

--- app/controllers/go_controller.rb
+++ app/controllers/go_controller.rb
@@ -302,8 +302,9 @@ class GoController < ApplicationController
 		# log this click 
 		go_key = params[:key].gsub('_', ' ')
 		golinks = ParseGoLink.where(key: go_key).to_a
-		email = params[:email] ? params[:email] : ''
-		email = current_member ? current_member.email : email
+		
+		email = current_member ? current_member.email : ''
+		email = params[:email] ? params[:email] : email
 		GoLog.log_click(email, go_key, Time.now)
 		if golinks.length > 0
 			# correctly used alias
