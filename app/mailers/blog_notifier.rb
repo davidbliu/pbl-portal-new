ca53ebfb0c3d7206bd1ec8bf18fdf9c35e137992
David Liu <davidbliu@gmail.com>
2015-09-13 23:37:10 -0700

    add mailing lists to notifier

--- app/mailers/blog_notifier.rb
+++ app/mailers/blog_notifier.rb
@@ -4,6 +4,7 @@ class BlogNotifier < ActionMailer::Base
   # send a signup email to the user, pass in the user object that   contains the user's email address
 
   def send_blog_email(members=['davidbliu@gmail.com'], post)
+  	puts members.join(',')
     @post = post
     mail( :to => members.join(','),
     :subject => 'PBL Blog: ' + post.title ).deliver
